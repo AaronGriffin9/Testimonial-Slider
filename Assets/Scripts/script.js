464929cc31f9333f3aa0cc6365a2f97cc8178789
AaronGriffin9 <44605619+AaronGriffin9@users.noreply.github.com>
2020-12-03 14:29:27 +0000

    Add files via upload

---
+++ Assets/Scripts/script.js
@@ -0,0 +1,13 @@
+// let next = document.getElementById("next");
+// let prev = document.getElementById("prev");
+// let next2 = document.getElementById("next2");
+// let prev2 = document.getElementById("prev2");
+// let john = document.getElementById("john");
+// let tanya = document.getElementById("tanya");
+//
+// next.addEventListener("click", nextclick);
+// prev.addEventListener("click", prevclick);
+//
+// function nextclick(){
+//   john.classList.toggle("display");
+// }
